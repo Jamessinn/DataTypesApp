8a7656f0d227cec76d17bcc5b48ae44bfc5a2da9
carrascal.296800 <carrascal.296800@LAB-B-PC11>
2024-02-16 09:39:46 +0800

    Add project files.

---
+++ DataTypesApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DataTypesApp
+{
+    class DataTypesProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the pieces of apple: ");
+            int applePieces = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter total of price of: " + applePieces +  " apple(s): ");
+            double applePrices = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The total price of " + applePieces + " apple(s) " + "is: " + applePrices);
+            Console.WriteLine("The value of converted price is: " + (int)applePrices);
+            Console.WriteLine("Press any key to exit...........");
+            Console.ReadKey();
+
+        }
+    }
+}
